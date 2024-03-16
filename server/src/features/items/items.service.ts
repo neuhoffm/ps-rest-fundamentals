9d5b9f6c0a6d1b78bb90ecb26e8af818077ba9c9
Markus Neuhoff <5299121+neuhoffm@users.noreply.github.com>
2024-03-16 15:14:42 -0400

    item list caching

--- server/src/features/items/items.service.ts
+++ server/src/features/items/items.service.ts
@@ -1,16 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 import { Item, ItemDTO, ItemDetail } from "../types";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import TTLCache from "@isaacs/ttlcache";
+
+const options = { ttl: 1000 * 60 * 60 * 24 };
+const cache = new TTLCache(options);
 
 const prisma = new PrismaClient();
+const listKey = "items-list";
 
 export function getItems(): Promise<Item[]> {
-  return prisma.item.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-  });
+  if (cache.has(listKey)) {
+    const cacheItems = cache.get<Item[]>(listKey);
+    if (cacheItems != undefined) {
+      return Promise.resolve(cacheItems);
+    }
+  }
+
+  return prisma.item
+    .findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+    .then((items) => {
+      cache.set(listKey, items);
+      return items;
+    });
 }
 
 export function getItemDetail(itemId: number): Promise<ItemDetail | null> {
@@ -20,19 +37,24 @@ export function getItemDetail(itemId: number): Promise<ItemDetail | null> {
 }
 
 export function upsertItem(item: ItemDTO): Promise<Item | null> {
-  return prisma.item.upsert({
-    where: {
-      id: item.id || -1,
-    },
-    update: {
-      name: item.name,
-      description: item.description,
-    },
-    create: {
-      name: item.name,
-      description: item.description,
-    },
-  });
+  return prisma.item
+    .upsert({
+      where: {
+        id: item.id || -1,
+      },
+      update: {
+        name: item.name,
+        description: item.description,
+      },
+      create: {
+        name: item.name,
+        description: item.description,
+      },
+    })
+    .then((item) => {
+      cache.delete(listKey);
+      return item;
+    });
 }
 
 export function deleteItem(itemId: number): Promise<Item | null> {
@@ -49,5 +71,9 @@ export function deleteItem(itemId: number): Promise<Item | null> {
       } else {
         throw error;
       }
+    })
+    .then((item) => {
+      cache.delete(listKey);
+      return item;
     });
 }
