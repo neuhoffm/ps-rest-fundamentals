514a1288a77cc7a1346a008981a102dae4654c66
Markus Neuhoff <5299121+neuhoffm@users.noreply.github.com>
2024-03-16 22:55:54 -0400

    updating v2 routes for access token

--- server/src/features/v2/routes.ts
+++ server/src/features/v2/routes.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { customersRouter } from "../v1/customers/customers.router";
 import { ordersRouter } from "../v1/orders/orders.router";
 import { itemsRouterV2 } from "./items/items.routerV2";
+import { validateAccessToken } from "../../middleware/auth0.middleware";
 
 // register routes
 export const v2Router = express.Router();
 
 v2Router.use("/items", itemsRouterV2);
 
-v2Router.use("/customers", customersRouter);
+v2Router.use("/customers", validateAccessToken, customersRouter);
 
-v2Router.use("/orders", ordersRouter);
+v2Router.use("/orders", validateAccessToken, ordersRouter);
